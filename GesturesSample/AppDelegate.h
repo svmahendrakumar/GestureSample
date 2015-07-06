66a07ec4f98ad20f0b251cfb23830abd8c7f1398
mahendra.kumar <mahendrakumar@DO.local>
2015-07-06 12:25:25 +0530

    Initial Commit

---
+++ GesturesSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GesturesSample
+//
+//  Created by mahendra.kumar on 06/07/15.
+//  Copyright (c) 2015 DreamOrbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GesturesSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturesSample
+//
+//  Created by mahendra.kumar on 06/07/15.
+//  Copyright (c) 2015 DreamOrbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
